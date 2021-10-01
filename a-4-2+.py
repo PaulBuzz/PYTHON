c3377b1931a6ed1325773d80bc14e6c66bd9cf36
PaulBuzz <84155505+PaulBuzz@users.noreply.github.com>
2021-10-01 19:38:15 +0300

    Add files via upload

---
+++ a-4-2+.py
@@ -0,0 +1,8 @@
+fruits = ["Strawberries", "Cherry", "Melons", "Oranges", "Bananas"]
+vegetables = ["Potatoes", "Onions", "Carrots", "Cabbages", "Pickles"]
+
+dozen = [fruits, vegetables]
+
+print(dozen[0][2])
+# [0] - номер списка
+# [2] - номер элемента в заданном ранее списке
