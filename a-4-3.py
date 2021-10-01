2f8a1171b864dabfd56519cad5aa20a5bd6edde0
PaulBuzz <84155505+PaulBuzz@users.noreply.github.com>
2021-10-01 19:38:30 +0300

    Add files via upload

---
+++ a-4-3.py
@@ -0,0 +1,13 @@
+row1 = ["⬜️", "⬜️", "⬜️"]
+row2 = ["⬜️", "⬜️", "⬜️"]
+row3 = ["⬜️", "⬜️", "⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+horiz = int(position[0])
+vert = int(position[1])
+
+map[vert - 1][horiz - 1] = ":)"
+
+print(f"{row1}\n{row2}\n{row3}")
