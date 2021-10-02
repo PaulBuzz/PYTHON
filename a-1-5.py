701aedad7e4ea62c0a88c57e23dde37ad876c613
PaulBuzz <84155505+PaulBuzz@users.noreply.github.com>
2021-10-02 20:55:20 +0300

    Update a-1-5.py

--- a-1-5.py
+++ a-1-5.py
@@ -1,9 +1,8 @@
 from typing import Reversible
 
-
 print("What your porn name would be? Let's check!")
 fruit = input("What is your favourite fruit?\n")
 animal = input("What is your favourite animal?\n")
-print("Your porn name is: " + fruit[::-1] + " " + animal[::-1])
+print("Your cartoon name is: " + fruit[::-1] + " " + animal[::-1])
 # [::-1] - реверс слова в строке
 
