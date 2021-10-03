1d6abc1fc0fc2963b4100e7de9208ffdb34e2a2d
PaulBuzz <84155505+PaulBuzz@users.noreply.github.com>
2021-10-03 15:41:13 +0300

    Add files via upload

---
+++ a-5-4.py
@@ -0,0 +1,18 @@
+# for num in range(1, 101):
+#     txt = ""
+#     if num % 3 == 0:
+#         txt += 'Fizz'
+#         # print(end="Fizz")
+#     if num % 5 == 0:
+#         txt += 'Buzz'
+#     if txt == '':
+#         txt = num
+#     print(txt)
+# perfect solution
+
+for num in range(1, 101):
+    if num % 3 == 0:
+        print(end="Fizz")
+    if num % 5 == 0:
+        print(end="Buzz")
+    print(num)
