9724f8fc8463484953b03ce5ea06dbd282c079de
PaulBuzz <84155505+PaulBuzz@users.noreply.github.com>
2021-10-03 15:41:25 +0300

    Add files via upload

---
+++ a-5-5.py
@@ -0,0 +1,42 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+pswd = ""
+
+for char in range(1, nr_letters + 1):
+    pswd += random.choice(letters)
+
+for sym in range(1, nr_symbols + 1):
+    pswd += random.choice(symbols)
+
+for num in range(1, nr_numbers + 1):
+    pswd += random.choice(numbers)
+print(f"Your new EASY password is: {pswd}")
+# easy pass without full random
+
+pswd_list = []
+
+for char in range(1, nr_letters + 1):
+    pswd_list += random.choice(letters)
+
+for sym in range(1, nr_symbols + 1):
+    pswd_list += random.choice(symbols)
+
+for num in range(1, nr_numbers + 1):
+    pswd_list += random.choice(numbers)
+random.shuffle(pswd_list)
+
+password = ""
+for char in pswd_list:
+    password += char
+print(f"Your new HARD password is: {password}")
+# hard pass with full random
