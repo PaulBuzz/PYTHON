bae977f519b09de70516f30935d2936ca96bdf0f
PaulBuzz <84155505+PaulBuzz@users.noreply.github.com>
2021-10-03 15:41:00 +0300

    Add files via upload

---
+++ a-5-3+.py
@@ -0,0 +1,12 @@
+# total = 0
+# for number in range(0, 101):
+#     if number % 2 == 0:
+#         total += number
+# print(total)
+# solution using if
+
+total = 0
+for number in range(2, 101, 2):
+    total += number
+print(total)
+# easier
