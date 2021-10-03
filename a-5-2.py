694a865b5b0d499f24b508bc169a93d18730dd7b
PaulBuzz <84155505+PaulBuzz@users.noreply.github.com>
2021-10-03 15:40:34 +0300

    Add files via upload

---
+++ a-5-2.py
@@ -0,0 +1,10 @@
+stud_score = input("Enter the list of scores: ").split(", ")
+for n in range(0, len(stud_score)):
+    stud_score[n] = int(stud_score[n])
+
+max_score = 0
+for score in stud_score:
+    if score > max_score:
+        max_score = score
+print(f"The highest score in the class is: {max_score}")
+# high score using for/in loop and without max() func
