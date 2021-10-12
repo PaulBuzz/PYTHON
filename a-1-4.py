4d48e9ed5e617163c4d5d6f2a96718aa703ee6be
PaulBuzz <84155505+PaulBuzz@users.noreply.github.com>
2021-10-12 21:27:43 +0300

    Update a-1-4.py

--- a-1-4.py
+++ a-1-4.py
@@ -3,5 +3,4 @@ b = input('Enter "b": ')
 print("And the numbers are majestically changed!")
 print("a = " + b)
 print("b = " + a)
-# меняет местами введенные числа
 # можно сделать через c=a, a=b, b=c, но и так сойдет
