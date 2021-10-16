1de9ec53b3f928d11886a34bef548b85841c505b
PaulBuzz <84155505+PaulBuzz@users.noreply.github.com>
2021-10-16 13:56:48 +0300

    Update and rename a-1-5.py to day1/1-1 (reverse-words-into-new-name).py

--- day1/1-1 (reverse-words-into-new-name).py
+++ day1/1-1 (reverse-words-into-new-name).py
@@ -1,8 +1,7 @@
 from typing import Reversible
 
-print("What your porn name would be? Let's check!")
+print("What your cartoon name would be? Let's check!\n")
 fruit = input("What is your favourite fruit?\n")
 animal = input("What is your favourite animal?\n")
 print("Your cartoon name is: " + fruit[::-1] + " " + animal[::-1])
 # [::-1] - реверс слова в строке
-
