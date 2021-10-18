1f70f0b95b8146ce6d62a7484ce93c5fcab90a6b
PaulBuzz <84155505+PaulBuzz@users.noreply.github.com>
2021-10-18 21:14:56 +0300

    Update and rename a-3-3.py to day3/3-3 (pizza-ordering).py

--- day3/3-3 (pizza-ordering).py
+++ day3/3-3 (pizza-ordering).py
@@ -4,29 +4,28 @@ add_pepper = input("Would you like some pepperoni? S(2$), M/L(3$) Y / N ")
 extra_cheese = input("Would you like some extra cheese? S/M/L(1$) Y / N ")
 bill = 0
 
-# if size == "S":
-#     bill = 15
-#     if add_pepper == "Y":
-#         bill += 2
-#     if extra_cheese == "Y":
-#         bill += 1
-#     print(f"Your total bill = {bill}$.")
-# if size == "M":
-#     bill = 20
-#     if add_pepper == "Y":
-#         bill += 3
-#     if extra_cheese == "Y":
-#         bill += 1
-#     print(f"Your total bill = {bill}$.")
-# if size == "L":
-#     bill = 25
-#     if add_pepper == "Y":
-#         bill += 3
-#     if extra_cheese == "Y":
-#         bill += 1
-#     print(f"Your total bill = {bill}$.")
+if size == "S":
+    bill = 15
+    if add_pepper == "Y":
+        bill += 2
+    if extra_cheese == "Y":
+        bill += 1
+    print(f"Your total bill = {bill}$.")
+if size == "M":
+    bill = 20
+    if add_pepper == "Y":
+        bill += 3
+    if extra_cheese == "Y":
+        bill += 1
+    print(f"Your total bill = {bill}$.")
+if size == "L":
+    bill = 25
+    if add_pepper == "Y":
+        bill += 3
+    if extra_cheese == "Y":
+        bill += 1
+    print(f"Your total bill = {bill}$.")
 # моё собственное решение
-
 
 # if size == "S":
 #     bill += 15
