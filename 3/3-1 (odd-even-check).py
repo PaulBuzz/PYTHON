39e20ccd2dd578c314b6a3048347af688db40811
PaulBuzz <84155505+PaulBuzz@users.noreply.github.com>
2021-10-23 13:55:26 +0300

    Update 3-1 (odd-even-check).py

--- 3/3-1 (odd-even-check).py
+++ 3/3-1 (odd-even-check).py
@@ -1,6 +1,6 @@
 print("Let's check if numbers are odd or even.")
 num = int(input("Enter your number here: "))
 if num % 2 == 0:
-    print("Number is even!")
+    print("Number is even.")
 else:
-    print("Number is odd!")
+    print("Number is odd.")
