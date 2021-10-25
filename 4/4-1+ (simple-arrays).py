06392dd30bb73aa68f2f91c10d28c7fd434dc79b
PaulBuzz <84155505+PaulBuzz@users.noreply.github.com>
2021-10-25 19:48:37 +0300

    Update 4-1+ (simple-arrays).py

--- 4/4-1+ (simple-arrays).py
+++ 4/4-1+ (simple-arrays).py
@@ -2,6 +2,5 @@ fruits = ["Strawberries", "Cherry", "Melons", "Oranges", "Bananas"]
 vegetables = ["Potatoes", "Onions", "Carrots", "Cabbages", "Pickles"]
 
 dozen = [fruits, vegetables]
-
 print(dozen[0][2])
 # [0] - номер списка, [2] - номер элемента в заданном ранее списке
