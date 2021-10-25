2fb3d06861e352a78c534561153f983acb7ee293
PaulBuzz <84155505+PaulBuzz@users.noreply.github.com>
2021-10-25 19:48:02 +0300

    Update 4-0 (simple-lovescore).py

--- 4/4-0 (simple-lovescore).py
+++ 4/4-0 (simple-lovescore).py
@@ -2,9 +2,7 @@ import random
 
 rand_int = random.randint(1, 20)
 print(rand_int)
-
 rand_float = random.random() * 5
 print(rand_float)
-
 love_score = random.randint(1, 100)
 print(f"Your love score is {love_score}.")
