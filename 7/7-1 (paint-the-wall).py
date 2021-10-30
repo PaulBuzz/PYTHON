2a9da9287c4b5549c9b7667ce0aac8967facfcab
PaulBuzz <84155505+PaulBuzz@users.noreply.github.com>
2021-10-30 18:28:27 +0300

    Update 7-1 (paint-the-wall).py

--- 7/7-1 (paint-the-wall).py
+++ 7/7-1 (paint-the-wall).py
@@ -2,13 +2,11 @@ import math
 
 print("Let's paint the wall and decide how many cans of paint we actually need.")
 
-
 def paint_calc(height, width, cover):
     area = height * width
     # math.ceil is used to upscale the result and we need it in this particular case to have enough paint
     cans = math.ceil(area / cover)
     print(f"You'll need {cans} cans of paint")
-
 
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
